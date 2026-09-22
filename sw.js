4a3aacf4e87524412d32ae2e8447e0a70fc45210
serdaryener67 <serdaryener67@gmail.com>
2026-09-22 14:47:16 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// ÖNEMLİ: her güncellemede bu sürüm numarasını artır, aksi halde cihazlar eski cache'i kullanmaya devam eder
+const CACHE_NAME = 'damla-v1';
+
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(key){ return key !== CACHE_NAME; })
+            .map(function(key){ return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request);
+    })
+  );
+});
